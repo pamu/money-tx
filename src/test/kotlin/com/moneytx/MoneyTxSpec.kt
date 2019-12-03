6ab4c869cf0544999e372824fccf04078e2c96df
npamu <nagarjuna.pamu@gmail.com>
2019-12-03 08:41:42 +0100

    Add more integration tests.

--- src/test/kotlin/com/moneytx/MoneyTxSpec.kt
+++ src/test/kotlin/com/moneytx/MoneyTxSpec.kt
@@ -4,17 +4,17 @@ import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
 import io.javalin.Javalin
-import io.kotlintest.Spec
 import io.kotlintest.specs.StringSpec
 import com.github.kittinunf.fuel.jackson.responseObject
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.moneytx.domain.Account
-import io.kotlintest.shouldBe
-import com.moneytx.domain.AccountId
-import com.moneytx.domain.Command
-import com.moneytx.domain.Money
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.Request
+import com.moneytx.domain.*
+import io.kotlintest.*
+import io.kotlintest.matchers.string.shouldContain
 import java.util.*
 
+@ExperimentalStdlibApi
 class MoneyTxSpec : StringSpec() {
 
     private lateinit var app: Javalin
@@ -25,12 +25,12 @@ class MoneyTxSpec : StringSpec() {
         app.start(9000)
     }
 
-    val base = "http://localhost:9000"
-    val createAccountEndPoint = "$base/createAccount"
-    val depositEndPoint = "$base/deposit"
-    val withdrawEndPoint = "$base/withdraw"
-    val transferEndPoint = "$base/transfer"
-    fun currentBalanceEndPoint(id: UUID) = "$base/currentBalance/$id"
+    private val base = "http://localhost:9000"
+    private val createAccountEndPoint = "$base/createAccount"
+    private val depositEndPoint = "$base/deposit"
+    private val withdrawEndPoint = "$base/withdraw"
+    private val transferEndPoint = "$base/transfer"
+    private fun currentBalanceEndPoint(id: UUID) = "$base/currentBalance/$id"
 
     init {
         "New account can be created" {
@@ -84,6 +84,150 @@ class MoneyTxSpec : StringSpec() {
 
             afterDeposit shouldBe acc.copy(currentBalance = amount)
         }
+
+        "Deposit to non-existent account not allowed" {
+            val id = UUID.randomUUID()
+            try {
+                depositExpectingFailure(id, Money(1.toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain AccountDoesNotExist(AccountId(id)).message!!
+            }
+        }
+
+        "Negative or zero deposits not allowed" {
+            try {
+                val acc = createNewAccount()
+                depositExpectingFailure(acc.id, Money((-1).toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain IllegalAmount.message!!
+            }
+
+            try {
+                val acc = createNewAccount()
+                depositExpectingFailure(acc.id, Money(0.toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain IllegalAmount.message!!
+            }
+        }
+
+        "Withdraw from non-existent account not allowed" {
+            val id = UUID.randomUUID()
+            try {
+                withdrawExpectingFailure(id, Money(1.toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain AccountDoesNotExist(AccountId(id)).message!!
+            }
+        }
+
+        "Negative or zero withdraws not allowed" {
+            try {
+                val acc = createNewAccount()
+                withdrawExpectingFailure(acc.id, Money((-1).toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain IllegalAmount.message!!
+            }
+
+            try {
+                val acc = createNewAccount()
+                withdrawExpectingFailure(acc.id, Money(0.toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain IllegalAmount.message!!
+            }
+        }
+
+        "Withdraw can be done only with sufficient funds" {
+            val acc = createNewAccount() // 0 funds
+            try {
+                withdrawExpectingFailure(acc.id, Money(100.toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain InsufficientFunds(
+                        AccountId(acc.id), acc.currentBalance).message!!
+            }
+        }
+
+        "Transfer from/to non-existent account not allowed" {
+            val acc = createNewAccount()
+            deposit(acc.id, Money(100.toBigDecimal()))
+            // Unknown payee
+            val randomID = UUID.randomUUID()
+            try {
+                transferExpectingFailure(accountId = acc.id, payee = randomID, amount = Money((10).toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain AccountDoesNotExist(
+                        accId = AccountId(randomID), isPayee = true).message!!
+            }
+
+            // Unknown account
+            try {
+                transferExpectingFailure(accountId = randomID, payee = acc.id, amount = Money((10).toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain AccountDoesNotExist(accId = AccountId(randomID)).message!!
+            }
+        }
+
+        "Negative or zero transfers not allowed" {
+            val acc = createNewAccount()
+            deposit(acc.id, Money(100.toBigDecimal()))
+            val payee = createNewAccount()
+            try {
+                transferExpectingFailure(acc.id, payee = payee.id, amount = Money((-1).toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain IllegalAmount.message!!
+            }
+
+            try {
+                transferExpectingFailure(acc.id, payee = payee.id, amount = Money((-1).toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain IllegalAmount.message!!
+            }
+        }
+
+        "Transfer can be done only with sufficient funds" {
+            val acc = createNewAccount() // Has zero amount
+            val payee = createNewAccount()
+            try {
+                transferExpectingFailure(acc.id, payee = payee.id, amount = Money(100.toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain InsufficientFunds(
+                        AccountId(acc.id), acc.currentBalance).message!!
+            }
+        }
+
+        "Self transfer not allowed" {
+            try {
+                val acc = createNewAccount()
+                transferExpectingFailure(acc.id, acc.id, Money(100.toBigDecimal()))
+                fail("Failure expected")
+            } catch (err: FuelError) {
+                err.message shouldContain "400"
+                err.errorData.decodeToString() shouldContain SelfTransfer.message!!
+            }
+        }
+
     }
 
     private fun createNewAccount(): Account {
@@ -92,35 +236,69 @@ class MoneyTxSpec : StringSpec() {
         return result.get()
     }
 
-    private fun deposit(accountId: UUID, amount: Money): Account {
-        val (_, res, result) = depositEndPoint.httpPost().jsonBody(
+    private fun depositReq(accountId: UUID, amount: Money): Request {
+        return depositEndPoint.httpPost().jsonBody(
                 jacksonObjectMapper().writeValueAsString(Command.Deposit(
                         accountId = AccountId(accountId),
                         amount = amount
-                ))).responseObject<Account>()
+                )))
+    }
+
+    private fun deposit(accountId: UUID, amount: Money): Account {
+        val (_, res, result) = depositReq(accountId, amount).responseObject<Account>()
         res.statusCode shouldBe 200
         return result.get()
     }
 
-    private fun withdraw(accountId: UUID, amount: Money): Account {
-        val (_, res, result) = withdrawEndPoint.httpPost().jsonBody(
+    private fun depositExpectingFailure(accountId: UUID, amount: Money): Unit {
+        val (_, res, result) = depositReq(accountId, amount)
+                .responseString()
+        res.statusCode shouldNotBe 200
+        throw result.component2() ?: return
+    }
+
+    private fun withdrawReq(accountId: UUID, amount: Money): Request {
+        return withdrawEndPoint.httpPost().jsonBody(
                 jacksonObjectMapper().writeValueAsString(Command.Withdraw(
                         accountId = AccountId(accountId),
                         amount = amount
-                ))).responseObject<Account>()
+                )))
+    }
+
+    private fun withdraw(accountId: UUID, amount: Money): Account {
+        val (_, res, result) = withdrawReq(accountId, amount).responseObject<Account>()
         res.statusCode shouldBe 200
         return result.get()
     }
 
-    private fun transfer(accountId: UUID, payee: UUID, amount: Money): Account {
-        val (_, res, result) = transferEndPoint.httpPost().jsonBody(
+    private fun withdrawExpectingFailure(accountId: UUID, amount: Money): Unit {
+        val (_, res, result) = withdrawReq(accountId, amount)
+                .responseString()
+        res.statusCode shouldNotBe 200
+        throw result.component2() ?: return
+    }
+
+    private fun transferReq(accountId: UUID, payee: UUID, amount: Money): Request {
+        return transferEndPoint.httpPost().jsonBody(
                 jacksonObjectMapper().writeValueAsString(Command.Transfer(
                         accountId = AccountId(accountId),
                         payee = AccountId(payee),
                         amount = amount
-                ))).responseObject<Account>()
+                )))
+    }
+
+    private fun transfer(accountId: UUID, payee: UUID, amount: Money): Account {
+        val (_, res, result) = transferReq(accountId = accountId, payee = payee, amount = amount)
+                .responseObject<Account>()
         res.statusCode shouldBe 200
         return result.get()
+    }
+
+    private fun transferExpectingFailure(accountId: UUID, payee: UUID, amount: Money): Unit {
+        val (_, res, result) = transferReq(accountId = accountId, payee = payee, amount = amount)
+                .responseString()
+        res.statusCode shouldNotBe 200
+        throw result.component2() ?: return
     }
 
     private fun currentBalance(accountId: UUID): Account {
